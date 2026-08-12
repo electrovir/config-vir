f6e64a9514079017d75df2531f30a3fc1562331a
electrovir <electrovir@users.noreply.github.com>
2026-08-12 12:52:46 +0000

    [major] restart from scratch

--- src/file-paths.test-helper.ts
+++
@@ -1,8 +0,0 @@
-import {dirname, join} from 'path';
-
-const repoRootDirPath = dirname(dirname(__filename));
-const testFilesDirPath = join(repoRootDirPath, 'test-files');
-
-export const testConfigs = {
-    basic: join(testFilesDirPath, 'basic-config.json'),
-};
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './define-config-file';
-export * from './definition';
-export * from './inputs';
-export * from './logging';
